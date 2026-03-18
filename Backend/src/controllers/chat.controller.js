9a9ab1bb3e55c51bebfaaaef91b6f0f61dd8603b
shivanshunigam <shivanshunigam6@gmail.com>
2026-03-18 12:15:51 +0530

    hello

--- Backend/src/controllers/chat.controller.js
+++ Backend/src/controllers/chat.controller.js
@@ -1,11 +1,49 @@
-import { generateResponse } from "../services/ai.service.js";
-export async function sendMessage(req, res) {
-    const { message } = req.body;
-    // Logic for sending message
+import { generateResponse,generateChatTitle } from "../services/ai.service.js";
+import chatModel from "../models/chat.model.js";
+import messageModel from "../models/message.model.js";
 
-    const result= await generateResponse(message);  
- res.json({
-    message: result
- })
 
-}
+export const sendMessage = async (req, res) => {
+
+    const { message,chat:chatId } = req.body;
+
+   let title=null, chat=null;
+   
+  if(!chatId){
+    title = await generateChatTitle(message);
+    chat = await chatModel.create({
+      user: req.user.id,
+      title
+    })
+
+  }
+
+  
+  const userMessage = await messageModel.create({
+    chat:chatId || chat._id,
+    content: message,
+    role: "user"
+  })
+
+
+  const messages = await messageModel.find({ chat: chatId });
+      const result = await generateResponse(messages);
+
+
+   
+      const aiMessage = await messageModel.create({
+        chat:chatId || chat._id,
+        
+        content: result,
+        role: "ai"
+      });
+
+    res.status(201).json({
+     
+      title,
+      chat,
+      aiMessage
+
+    });
+  
+};
