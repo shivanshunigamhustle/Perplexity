7d60b8af52f04382baddb4d5c30163eac3d46624
shivanshunigam <shivanshunigam6@gmail.com>
2026-03-26 12:03:10 +0530

    fix email smtp port 587 for render

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -1,25 +1,31 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         type: 'OAuth2',
         user: process.env.GOOGLE_USER,
+        clientId: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-        clientId: process.env.GOOGLE_CLIENT_ID
     }
 })
 
 transporter.verify()
-    .then(() => { console.log("Email transporter is ready to send emails"); })
-    .catch((err) => { console.error("Email transporter verification failed:", err); });
+    .then(() => { 
+        console.log("Email transporter is ready to send emails"); 
+    })
+    .catch((err) => { 
+        console.error("Email transporter verification failed:", err); 
+    });
 
 
 export async function sendEmail({ to, subject, html, text }) {
 
     const mailOptions = {
-        from: process.env.GOOGLE_USER,
+        from: `"Perplexity" <${process.env.GOOGLE_USER}>`,
         to,
         subject,
         html,
@@ -27,5 +33,6 @@ export async function sendEmail({ to, subject, html, text }) {
     };
 
     const details = await transporter.sendMail(mailOptions);
-    console.log("Email sent:", details);
+    console.log("Email sent:", details.messageId);
+    return details;
 }
